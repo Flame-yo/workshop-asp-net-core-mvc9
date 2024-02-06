033b3c7bf36d0ece30e5cf2f863a1c3a8f0f8bfc
Gustavo Henrique <gustavohffs@gmail.com>
2024-02-06 17:44:57 -0300

    Project Created

---
+++ SalesWebMvc9/Controllers/DepartmentsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc9.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc9.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> list = new List<Department>();
+            list.Add(new Department { Id = 1, Name = "Computers" });
+            list.Add(new Department { Id = 2, Name = "Fashion" });
+
+            return View(list);
+        }
+    }
+}
---
+++ SalesWebMvc9/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc9.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc9.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Sales Web Mvc Project C#";
+            ViewData["Email"] = "nelio@gmail.com";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
